17fe17c8307d1115e522fcbc308908372bab2368
Vitaly Korobov <info@isvk.ru>
2019-11-22 20:22:08 +0300

    function request for API

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,24 +1,40 @@
 import { IHttp } from "./IHttp";
 
+interface IData {
+    memberId?: string | null;
+    token?: string | null;
+    key?: string | null;
+    [key: string]: string | number | null | undefined;
+}
+
 export default class Http implements IHttp {
-    get = (url: string, data: object) => {
+    get = (url: string, data: IData) => {
         return this.request("GET", url, data);
     };
 
-    post = (url: string, data: object) => {
+    post = (url: string, data: IData) => {
         return this.request("POST", url, data);
     };
 
-    put = (url: string, data: object) => {
+    put = (url: string, data: IData) => {
         return this.request("PUT", url, data);
     };
 
-    delete = (url: string, data: object) => {
+    delete = (url: string, data: IData) => {
         return this.request("DELETE", url, data);
     };
 
-    request = (method: string, url: string, data = {}) => {
-        return fetch("http://localhost:8000/api/" + url, {
+    request = (method: string, url: string, data: IData = {}) => {
+        if (!data.memberId) data.memberId = localStorage.getItem("memberId");
+        if (!data.token) data.token = localStorage.getItem("token");
+        if (!data.key) data.key = process.env.REACT_APP_API_KEY;
+
+        const parameters = Object.keys(data)
+            .filter(key => typeof data[key] === "string")
+            .map(key => key + "=" + data[key])
+            .join("&");
+
+        return fetch("https://api.trello.com/1/" + url + "?" + parameters, {
             method
         }).then(response => response.json());
     };
