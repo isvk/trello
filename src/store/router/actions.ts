b998a74242545987672fa580b2f6adc40942d550
Vitaly Korobov <info@isvk.ru>
2019-12-09 18:19:05 +0300

    feat. store.router redirect

---
+++ src/store/router/actions.ts
@@ -0,0 +1,15 @@
+import { push } from "connected-react-router";
+
+export const redirectToAuthForGetToken = () => {
+    if (process.env.REACT_APP_API_KEY) {
+        document.location.href =
+            "https://trello.com/1/authorize?expiration=1day&name=TestTrello&return_url=http://localhost:3000/auth/save_token/&scope=read&response_type=token&key=" +
+            process.env.REACT_APP_API_KEY;
+    } else {
+        console.error("Not specified env.REACT_APP_API_KEY");
+    }
+};
+
+export const redirectToBoard = (idBoard: string) => {
+    return push("/board/" + idBoard);
+};
