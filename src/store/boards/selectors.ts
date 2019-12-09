7a363691156ada73f2dc555ba503b08c62083a1c
Vitaly Korobov <info@isvk.ru>
2019-12-09 16:21:20 +0300

    feat. ReSelect add selectors

---
+++ src/store/boards/selectors.ts
@@ -0,0 +1,5 @@
+import { TStoreBoard } from "./reducer";
+
+export const getBoardsLoadingState = (state: TStoreBoard) =>
+    state.boardsLoadingState;
+export const getBoards = (state: TStoreBoard) => state.collection;
--- src/store/rootSelector.ts
+++ src/store/rootSelector.ts
@@ -1,4 +1,33 @@
 import { createSelector } from "reselect";
-import { IStore } from "./rootReducer";
+import { IStore } from "src/store/rootReducer";
+import { getIdMember, getToken, getKey } from "src/store/auth/selectors";
+import { getBoards } from "src/store/boards/selectors";
 
 const propsFirstSelector = (_: IStore, prop: any) => prop;
+
+export const authState = (state: IStore) => state.auth;
+export const authGetIdMember = createSelector(authState, getIdMember);
+export const authGetToken = createSelector(authState, getToken);
+export const authGetKey = createSelector(authState, getKey);
+
+export const boardState = (state: IStore) => state.boards;
+export const boardsGetBoards = createSelector(boardState, getBoards);
+export const boardsGetBoardById = createSelector(
+    boardsGetBoards,
+    propsFirstSelector,
+    (boards, idBoard) => boards.get(idBoard)
+);
+
+export const listState = (state: IStore) => state.lists;
+export const listsGetListsByIdBoard = createSelector(
+    listState,
+    propsFirstSelector,
+    (lists, idBoard) => lists.filter(list => list.idBoard === idBoard)
+);
+
+export const cardState = (state: IStore) => state.cards;
+export const cardsGetCardsByIdList = createSelector(
+    cardState,
+    propsFirstSelector,
+    (cards, idList) => cards.filter(card => card.idList === idList)
+);
