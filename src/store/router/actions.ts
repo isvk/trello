1930630edea46d3be97a0e4014e395dff7ff0034
Vitaly Korobov <info@isvk.ru>
2019-12-10 20:44:37 +0300

    fix. trello auth scope=write

--- src/store/router/actions.ts
+++ src/store/router/actions.ts
@@ -3,7 +3,7 @@ import { push } from "connected-react-router";
 export const redirectToAuthForGetToken = () => {
     if (process.env.REACT_APP_API_KEY) {
         document.location.href =
-            "https://trello.com/1/authorize?expiration=1day&name=TestTrello&return_url=http://localhost:3000/auth/save_token/&scope=read&response_type=token&key=" +
+            "https://trello.com/1/authorize?scope=write&expiration=1day&name=TestTrello&return_url=http://localhost:3000/auth/save_token/&scope=read&response_type=token&key=" +
             process.env.REACT_APP_API_KEY;
     } else {
         console.error("Not specified env.REACT_APP_API_KEY");
