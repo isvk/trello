4ffa086a038c4eabf234c60121a9ca59094a55e8
isvk <info@isvk.ru>
2020-02-29 22:23:41 +0300

    feat. design page list boards

--- src/components/ListBoardsPage/ListBoardsPage.tsx
+++ src/components/ListBoardsPage/ListBoardsPage.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { TStoreBoard } from "src/store/boards/reducer";
+import { User } from "styled-icons/heroicons-outline/User";
+import { HomeAlt as Home } from "styled-icons/boxicons-regular/HomeAlt";
+
+import "./ListBoardsPage.scss";
+import Logout from "../Auth/Logout/Logout";
 
 interface ListBoardsPageProps {
     boards: TStoreBoard;
@@ -8,15 +13,30 @@ interface ListBoardsPageProps {
 
 export default function ListBoardsPage(props: ListBoardsPageProps) {
     return (
-        <div>
-            <h1>List Boards</h1>
-            <ul>
-                {props.boards.collection.valueSeq().map(board => (
-                    <li key={board.id}>
-                        <Link to={"/board/" + board.id}>{board.name}</Link>
-                    </li>
-                ))}
-            </ul>
+        <div className="pageListBoards">
+            <div className="head">
+                <Link to="/" className="linkHome">
+                    <Home size="20" className="iconHome" />
+                </Link>
+                <Logout />
+            </div>
+            <div className="wrapper-content">
+                <div className="contentPageListBoards">
+                    <h1 className="title">
+                        <User size="18" className="icon" />
+                        Выберите доску
+                    </h1>
+                    <ul className="listLinkBoards">
+                        {props.boards.collection.valueSeq().map(board => (
+                            <li key={board.id}>
+                                <Link to={"/board/" + board.id} className="linkBoard">
+                                    {board.name}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
