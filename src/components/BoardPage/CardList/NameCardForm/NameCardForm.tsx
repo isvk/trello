901c5cf006fa92f9dda9d86923215ffdcaec99bf
isvk <info@isvk.ru>
2020-03-05 18:55:49 +0300

    feat. focus textarea

--- src/components/BoardPage/CardList/NameCardForm/NameCardForm.tsx
+++ src/components/BoardPage/CardList/NameCardForm/NameCardForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Close } from "styled-icons/material/Close";
 
 import "./NameCardForm.scss";
@@ -12,12 +12,19 @@ interface ICreateCardFormProps {
 }
 
 export default function NameCardForm(props: ICreateCardFormProps) {
+    let textInput = useRef<HTMLTextAreaElement>(null);
+
+    useEffect(() => {
+        if (textInput.current) textInput.current.focus();
+    }, []);
+
     return (
         <div className="nameCardForm">
             <textarea
                 value={props.name}
                 onChange={e => props.handleChangeName(e.target.value)}
                 placeholder="Ввести заголовок для этой карточки"
+                ref={textInput}
             />
             <div className="panel">
                 <button className="saveCardButton" onClick={props.handleButtonSave}>
